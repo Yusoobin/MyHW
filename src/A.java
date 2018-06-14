0709081d0ef19338cbb33a09566b06d498750268
unknown <subeen2150@gmail.com>
2018-06-14 15:11:08 +0900

    Yusoobin

--- src/A.java
+++ src/A.java
@@ -2,4 +2,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("A!");
 	}
+	public static void main(String[] args) {
+		System.out.println("modify A!");
+	}
 }
--- src/C.java
+++ src/C.java
@@ -2,4 +2,7 @@ public class C {
 	public static void main(String[] args) {
 		System.out.println("C!");
 	}
+	public static void main(String[] args) {
+		System.out.println("modify C!");
+	}
 }
